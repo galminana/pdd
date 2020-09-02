302279bcbb01ad8b4558c0d2f21104f19cdbc950
Gabriel Almiñana <r470n@yahoo.com.ar>
2020-09-01 22:32:56 -0300

    clase 01/09/2020

---
+++ 20200825/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 1;
+            string clave = "";
+            while (clave!="secreto" && intentos<4)
+            {
+                Console.Write($"Ingrese la contrasena:{clave} ");
+                clave = Console.ReadLine();
+                int valor = int.Parse(clave);
+                intentos++;
+            }
+
+            //do
+            //{
+            //    Console.WriteLine(numero);
+            //} while (numero < 10);
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
---
+++ 20200901/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int secreto = random.Next(10);
+            int numero = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                numero = IngreseElNumero("Ingrese el numero: ");
+                if (numero == secreto) break;
+            }
+
+            if(2020 % 4 == 0)
+            {
+
+            }
+
+            string resultado;
+            if (numero == secreto)
+            {
+                resultado = Ganaste();
+            }
+            else
+            {
+                resultado = Perdiste(secreto);
+            }
+            Console.WriteLine(resultado);
+
+        }
+
+        static int IngreseElNumero(string mensaje)
+        {
+            Console.Write(mensaje);
+            string valor = Console.ReadLine();
+            return int.Parse(valor);
+
+        }
+
+        static string Ganaste()
+        {
+            string mensaje = "Ganaste";
+            return mensaje;
+        }
+
+        static string Perdiste(int valor)
+        {
+            return "Perdiste el numero era " + valor;
+        }
+    }
+}
